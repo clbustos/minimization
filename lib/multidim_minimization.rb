e538cc35763c2ba73fd6428564a1f4ec6da202c7
lasandun <lasandun@gmail.com>
2014-08-12 11:04:43 +0530

    now multidimensional minimization comes under 'minimization'

--- lib/multidim_minimization.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'multidim/brent_root_finder'
-require_relative 'multidim/conjugate_gradient'
-require_relative 'multidim/nelder_mead'
-require_relative 'multidim/point_value_pair'
-require_relative 'multidim/powell'
